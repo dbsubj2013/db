ab3e96cfc4d828f588ae1d0cbb7554dd079f5dfe
ttanachot <StupidCupid@StupidCupid.local>
2013-12-16 01:51:51 +0700

    Fix Bug

--- project/pop_hotel.php
+++ project/pop_hotel.php
@@ -37,7 +37,6 @@
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown">Information<b class="caret"></b></a>
                   <ul class="dropdown-menu">
                     <li class="nav-header">Facilities</li>
-                    <li><a href="transportation.php">Transportation</a></li>
                     <li><a href="hotel.php">Hotel</a></li>
                     <li><a href="Restaurant.php">Restaurant</a></li>            
                     <li class="divider"></li>
